61b5d905ab1c9298f58c1d5b8d6f4323db5292c0
TuPham <tupv@vnpt.vn>
2022-12-06 08:50:36 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    var html = fs.readFileSync(__dirname + '/index.html');
+    res.send(html)
 })
+
+var aasa = fs.readFileSync(__dirname + '/apple-app-site-association');
+app.get('/apple-app-site-association', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.set('Content-Length', aasa.size);
+    res.status(200).send(aasa);
+});
+
 app.listen(process.env.PORT || 3000)
